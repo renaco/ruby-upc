8f95ed5476d29b8f945d6ca224ba6be87fcc2430
Renaco <renanromero@gmail.com>
2015-09-08 21:33:33 -0500

    feat(site) my fisrt app

---
+++ calcularpromedio.rb
@@ -0,0 +1,11 @@
+# encoding:utf-8
+puts "ingrese la nota 1"
+nota1=gets.to_f
+puts "ingresar la nota 2"
+nota2=gets.to_f
+puts "ingrese la nota 3"
+nota3=gets.to_f
+
+promedio=(nota1+nota2+nota3)/3
+
+puts "El promedio es ="+promedio.to_s
